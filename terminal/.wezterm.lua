31ef59ab5986c5de19161f2d607e20177536fe4c
HyKo <punk.flag8609@fastmail.com>
2025-02-11 18:47:12 -0500

    Refine WezTerm configuration with visual and functional tweaks

--- terminal/.wezterm.lua
+++ terminal/.wezterm.lua
@@ -1,23 +1,16 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 local act = wezterm.action
--- local mux = wezterm.mux
--- This will hold the configuration.
 local config = wezterm.config_builder()
--- local gpus = wezterm.gui.enumerate_gpus()
--- config.webgpu_preferred_adapter = gpus[1]
--- config.front_end = "WebGpu"
 
 config.front_end = "OpenGL"
 config.max_fps = 144
 config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 1
 config.cursor_blink_rate = 500
-config.term = "xterm-256color" -- Set the terminal type
+config.term = "xterm-256color"
 
--- config.font = wezterm.font("Iosevka Custom")
 config.cell_width = 0.9
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 config.prefer_egl = true
 config.font_size = 11.0
 
@@ -30,16 +23,15 @@ config.window_padding = {
 
 -- tabs
 config.hide_tab_bar_if_only_one_tab = false
-config.use_fancy_tab_bar = false
--- config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = false
+config.show_tab_index_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
 
--- config.inactive_pane_hsb = {
--- 	saturation = 0.0,
--- 	brightness = 1.0,
--- }
-
--- This is where you actually apply your config choices
---
+config.inactive_pane_hsb = {
+ 	saturation = 0.0,
+ 	brightness = 1.0,
+}
 
 -- color scheme toggling
 wezterm.on("toggle-colorscheme", function(window, pane)
@@ -52,7 +44,6 @@ wezterm.on("toggle-colorscheme", function(window, pane)
 	window:set_config_overrides(overrides)
 end)
 
--- keymaps
 config.keys = {
 	{
 		key = "E",
@@ -97,20 +88,6 @@ config.keys = {
 	},
 	{ key = "9", mods = "CTRL", action = act.PaneSelect },
 	{ key = "L", mods = "CTRL", action = act.ShowDebugOverlay },
-	{
-		key = "O",
-		mods = "CTRL|ALT",
-		-- toggling opacity
-		action = wezterm.action_callback(function(window, _)
-			local overrides = window:get_config_overrides() or {}
-			if overrides.window_background_opacity == 1.0 then
-				overrides.window_background_opacity = 0.9
-			else
-				overrides.window_background_opacity = 1.0
-			end
-			window:set_config_overrides(overrides)
-		end),
-	},
 }
 
 config.mouse_bindings = {
@@ -129,19 +106,16 @@ config.mouse_bindings = {
 	},
 }
 
--- For example, changing the color scheme:
 config.color_scheme = "Cloud (terminal.sexy)"
 config.colors = {
-	background = "#06060F", -- dark purple
+	background = "#000000",
 	cursor_border = "#A3C0C7",
-	-- cursor_fg = "#281733",
 	cursor_bg = "#A3C0C7",
-	-- selection_fg = '#281733',
 
 	tab_bar = {
-		background = "#06060F",
+		background = "#000000",
 		active_tab = {
-			bg_color = "#06060F",
+			bg_color = "#000000",
 			fg_color = "#A3C0C7",
 			intensity = "Normal",
 			underline = "None",
@@ -149,7 +123,7 @@ config.colors = {
 			strikethrough = false,
 		},
 		inactive_tab = {
-			bg_color = "#06060F",
+			bg_color = "#000000",
 			fg_color = "#f8f2f5",
 			intensity = "Normal",
 			underline = "None",
@@ -158,20 +132,24 @@ config.colors = {
 		},
 
 		new_tab = {
-			bg_color = "#06060F",
+			bg_color = "#000000",
 			fg_color = "white",
 		},
 	},
 }
 
 config.window_frame = {
-	-- font = wezterm.font({ family = "Iosevka Custom", weight = "Regular" }),
-	active_titlebar_bg = "#06060F",
+	active_titlebar_bg = "#000000",
 }
 
 config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.default_prog = { "powershell.exe", "-NoLogo"}
 config.initial_cols = 80
 
--- and finally, return the configuration to wezterm
+-- Disable tab title
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local date = wezterm.strftime("%Y-%m-%d %H:%M")
+	return " " .. date .. " "
+end)
+
 return config
